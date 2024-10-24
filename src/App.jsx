41a50adb564aff6767007ea5ded0c0d6ac5cbebd
Trunng5703 <trinhhatrung69@gmail.com>
2024-10-24 10:14:06 +0700

    Account Menu

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Footer from './components/Footer/Footer'
+import { Account } from '@toolpad/core'
+import AccountMenu from './components/AccountMenu/AccountMenu'
 
 function App() {
 
   return (
     <>
+      <AccountMenu/>
       <Footer/>
     </>
   )
